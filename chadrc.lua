f22e85e0657b3493808da9561fac7b5a8b10922d
Redyf <mateusalvespereira7@gmail.com>
2023-06-04 22:44:41 -0300

    Discord presence timer reset fix

--- chadrc.lua
+++ chadrc.lua
@@ -64,7 +64,24 @@ M.ui = {
   },
 }
 
+-- M.settings = {
+--   cc_size = "80", -- Color column neither pass list or string
+--   so_size = 10,   -- Scrolloff amount
+--
+--   blacklist = {
+--     "NvimTree",
+--     "nvdash",
+--     "nvcheatsheet",
+--     "terminal",
+--     "Trouble",
+--     "help",
+--   },               -- Where to disable scrolloff and colorcolumn
+--
+--   discord_rpc = 2, -- Value on how many nvim processes load presence.lua (1 - neovide, 2 - neovim)
+-- }
+
 M.plugins = "custom.plugins"
 M.mappings = require "custom.mappings"
+M.settings = "custom.extras"
 
 return M
--- init.lua
+++ init.lua
@@ -1,8 +1,12 @@
 require "custom.extras"
-require "custom.neovide"
+-- require "custom.neovide"
 
 local opt = vim.opt
 local g = vim.g
 
 -- Vim options
 opt.timeoutlen = 10 -- Which-key timeout in ms
+opt.backup = false
+opt.swapfile = false
+opt.encoding = "utf-8"
+opt.fileencoding = "utf-8"
