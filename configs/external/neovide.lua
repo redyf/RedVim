87cd444a02f6995653306498fc1252c6ab74868d
Redyf <mateusalvespereira7@gmail.com>
2023-07-05 21:54:29 -0300

    Autosave toggle implemented

--- configs/external/neovide.lua
+++ configs/external/neovide.lua
@@ -12,10 +12,11 @@ if vim.g.neovide then
   -- vim.g.neovide_refresh_rate_idle = 5 -- This might not have an effect on every platform (e.g. Wayland).
 
   -- Neovide Fonts
-  -- vim.o.guifont = "Maple Mono NF:h12:b"
-  -- vim.o.guifont = "CaskaydiaCove Nerd Font:h12:b"
-  -- vim.o.guifont = "JetBrainsMono Nerd Font:h12:b"
-  -- vim.o.guifont = "FiraCode Nerd Font:h12:b"
-      vim.o.guifont = "Liga SFMono Nerd Font:h12:b"
-  -- vim.o.guifont = "BlexMono Nerd Font Mono:h12:b"
+  -- vim.o.guifont = "Maple Mono NF:h14:b"
+  -- vim.o.guifont = "CaskaydiaCove Nerd Font:h14:b"
+  -- vim.o.guifont = "JetBrainsMono Nerd Font:h13:b"
+  -- vim.o.guifont = "FiraCode Nerd Font:h14:b"
+  -- vim.o.guifont = "Liga SFMono Nerd Font:h13:b"
+  -- vim.o.guifont = "BlexMono Nerd Font Mono:h14:b"
+  vim.o.guifont = "Liga SFMono Nerd Font,JetBrainsMono Nerd Font:h13:b"
 end
--- init.lua
+++ init.lua
@@ -11,3 +11,35 @@ opt.swapfile = false
 opt.encoding = "utf-8"
 opt.fileencoding = "utf-8"
 opt.shiftwidth = 2
+
+local create_cmd = vim.api.nvim_create_user_command
+
+create_cmd("AutosaveToggle", function()
+  vim.api.nvim_echo({}, false, {})
+  vim.g.autosave = not vim.g.autosave
+
+  if vim.g.autosave then
+    vim.api.nvim_create_autocmd({ "InsertLeave", "TextChanged" }, {
+      group = vim.api.nvim_create_augroup("Autosave", {}),
+      callback = function()
+        -- save files only
+        if vim.api.nvim_buf_get_name(0) and #vim.bo.buftype == 0 then
+          vim.cmd "silent w"
+
+          vim.api.nvim_echo(
+            { { "󰄳", "LazyProgressDone" }, { " file autosaved at " .. os.date "%I:%M %p" } },
+            false,
+            {}
+          )
+
+          -- clear msg after 500ms
+          vim.defer_fn(function()
+            vim.api.nvim_echo({}, false, {})
+          end, 800)
+        end
+      end,
+    })
+  else
+    vim.api.nvim_del_augroup_by_name "Autosave"
+  end
+end, {})
